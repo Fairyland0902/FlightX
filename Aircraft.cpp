b1c003c83a311cf48a924dc94807722bb209ea2a
Zhang Xiangyu <me@zhangxiangyu.me>
2017-12-28 22:22:09 +0800

    瞎写一堆物理模拟 真的是瞎写的 有好多参数是随便搞得 因为我到现在还没把那堆Shader编译对，所以暂时也还没测试，也不知道旋转是不是反的

--- Aircraft.cpp
+++ Aircraft.cpp
@@ -1,12 +1,57 @@
 #include "aircraft.h"
 
+float Aircraft::getLength(const glm::vec3& v) {
+	return sqrt(v.x*v.x + v.y*v.y + v.z + v.z);
+}
 
+glm::vec3 Aircraft::getAcceleration() {
+	glm::vec3 acc(-airspeed*0.2f+ thrust*Front);
+	acc.y -= 0.5;
+	acc += Up * ias*ias*0.3f;
+	if(!inAir) {
+		if (acc.y < 0)acc.y = 0;
+	}
+	return acc;
+}
 
-Aircraft::Aircraft(string modelfile) :Model(modelfile),Camera()
+Aircraft::Aircraft(string modelfile) :Model(modelfile),Camera(),inAir(false)
 {
+	Position = glm::vec3(0.0f);
+	WorldUp = glm::vec3(0, 1, 0);
+	Front = glm::vec3(1, 0, 0);
+	Up = glm::vec3(0, 1, 0);
+	Right = glm::cross(Front, Up);
+	airspeed = glm::vec3(0,0,0);
 }
 
 
 Aircraft::~Aircraft()
 {
+
 }
+void WorldUpRotate(glm::vec3& v,double val) {
+	double cosv = cos(val), sinv=sin(val);
+	float vx = v.x, vz = v.z;
+	v.x = vx * cosv - vz * sinv;
+	v.z = vx * sinv + vz * cosv;
+}
+void Aircraft::Update(float dt) {
+	glm::vec3  a = getAcceleration();
+	auto yaw=glm::dot(a, Right)/(ias+0.05);
+	WorldUpRotate(Up, yaw);
+	WorldUpRotate(Front, yaw);
+	WorldUpRotate(Right, yaw);
+	Position += airspeed * dt;
+	airspeed += a * dt;
+	thrust += ((target_thrust>20?target_thrust:20) - thrust) *dt *0.25 ;
+	ias = getLength(airspeed);
+}
+
+const glm::vec3&& Aircraft::getAirspeed() {
+	return std::move(airspeed);
+}
+
+void Aircraft::setAirspeed(const glm::vec3& v) {
+	airspeed = v;
+}
+
--- Aircraft.h
+++ Aircraft.h
@@ -1,13 +1,24 @@
 #pragma once
 #include "model.h"
 #include "camera.h"
+#include <glm/detail/type_mat.hpp>
 
 class Aircraft :
 	public Model,
 	public Camera
 {
+	bool inAir;
+	float thrust;
+	float target_thrust;
+	glm::vec3 airspeed;
+	float ias;
+	float getLength(const glm::vec3&);
+	glm::vec3 getAcceleration();
 public:
-	Aircraft(string);
+	Aircraft(string modelname);
 	~Aircraft();
+	void Update(float deltaTime);
+	const glm::vec3&& getAirspeed();
+	void setAirspeed(const glm::vec3&);
 };
 
